b01b9c9a2e4e7e4b1fe09972da298ac04efea5d5
Ricardas23 <ric.ivanovas@gmail.com>
2023-10-04 19:58:23 +0300

    .

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// let sections = document.querySelectorAll('section')
-// let navLinks = document.querySelectorAll('header ul li a')
-
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY
-//         let offset = sec.offsetTop
-//         let height = sec.offsetHeight
-//         let id = sec.getAttribute('id')
-
-//         if(top >= offset && top < offset + height) {
-//             navLinks.forEach(links => {
-//                 document.querySelector('header ul li a [href*=' + id + ']')
-//             })
-//         }
-//     })
-// }
 
 
 let menu = document.querySelector('#menu-icon');
